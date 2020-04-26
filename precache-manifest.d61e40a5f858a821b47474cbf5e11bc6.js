daebb63d98d583006c9a767456ac44128bccc452
NChicken <nchicken1009@gmail.com>
2020-04-27 03:55:03 +0900

    Initial Commit

---
+++ precache-manifest.d61e40a5f858a821b47474cbf5e11bc6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b033613b457ae0c92cc57d4325e109b5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ffc08c127a19b0ccac55",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "6513330a5828a3cb135b",
+    "url": "/static/js/2.3008088c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.3008088c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ffc08c127a19b0ccac55",
+    "url": "/static/js/main.b95c31e7.chunk.js"
+  },
+  {
+    "revision": "e9a8ef28169e92b23dfd",
+    "url": "/static/js/runtime-main.b6ed7ae3.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
